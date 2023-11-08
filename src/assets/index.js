8d08af77a6220b8852b0486eb3eb747f4bd350ca
abduljeleelng <abduljeleelng@gmail.com>
2023-11-09 00:00:58 +0100

    App assets

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
+
+
+export const VERTICAL = hp('5%')
+
+export const HORIZONTAL = wp('5%')
+
+
+export const IMAGES = {
+    logo: require('./images/fastamoni.png'),
+}
